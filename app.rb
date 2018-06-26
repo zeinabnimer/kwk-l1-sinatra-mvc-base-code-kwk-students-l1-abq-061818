0d0296738e46b7cb1b4f9db4add55c792c5dd1ec
zeinabnimer <www.zeinab6@gmail.com>
2018-06-26 17:07:50 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 class IdeaBoxApp < Sinatra::Base
   get '/' do
-    "Hello, World!"
+    "<h1>Hello, World!</h1><blockquote>I guess I always felt even if the world came to an end, 
+    McDonald's would still be open.<cite>Susan Beth Pfeffer</cite></blockquote>"
   end
   end
